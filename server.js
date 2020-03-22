81f25733e880cc9a14c7b56fafd08df659082cee
Shirley Wong <swong@theparcelport.com>
2020-03-22 12:20:44 -0400

    changes for deploy

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const userRouter = require('./api/routes/users/userRoutes');
 const notesRouter = require('./api/routes/notes/notesRoutes');
 
+const PORT = process.env.PORT || 8080
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/note-app'
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,20 +16,22 @@ app.use('/api/notes', notesRouter);
 
 // for ALL routes that don't match, serve our react bundle.
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', function(req, res) {
+//after we build, ask expres to look at the build folder instead of running react server
+//localhost:3000 don't need to run anymore, just hit localhost:8080, it will serve both front and back
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
 mongoose
-  .connect('mongodb://localhost:27017/note-app',
+  .connect(MONGODB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
   )
   .then(() => {
-    app.listen('8080', () => {
+    app.listen(PORT, () => {
       console.log('server is running on port 8080');
     });
   })
